d0eb137e1d19ca0f8fb833e6e488aaa588209a83
Muad2003 <anuphap2003118@gmail.com>
2023-09-08 19:47:01 +0700

    updatehomework 9-1

--- Homework/HW09/hw09-1-input-value-into-array.cpp
+++ Homework/HW09/hw09-1-input-value-into-array.cpp
@@ -25,3 +25,35 @@
         Array = 1 1 3 9 8 2 7 8
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int array[ 100 ] ;
+    int n = 0 , k = 0 ;
+
+    while ( k != -1 )
+    {
+        printf ( "Input [%d] :\n", n ) ;
+        scanf ( "%d", &array[ n ] ) ;
+        k = array [ n ]  ;
+        n++ ;
+    }
+    
+    printf ( "Index =" ) ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        if ( array[ i ] == -1 ) {
+            break ;
+        }// end if ตรวจสอบถ้าเจอ -1
+        printf ( " %d", i ) ;
+    }// end for แสดง Index
+
+    printf ( "\nArray =" ) ;
+    for (int i = 0 ; i < n ; i++) { // แสดง Array
+        if ( array[ i ] == -1 ) { // ใช้เพื่อถ้าเจอ -1 ไม่ต้องให้แสดง
+            break ;
+        }// end if
+        printf ( " %d", array[ i ] ) ;
+    }// end for แสดง Array
+    return 0;
+}
